e7028f27bd12671bd26e8044be0e0ac78fb1e284
nxvenue <cenentury0941@gmail.com>
2025-03-03 02:12:04 +0530

    updated urls

--- venueflow-webapp/src/utils/urls.js
+++ venueflow-webapp/src/utils/urls.js
@@ -1,4 +1,5 @@
 const system_id = "02181f8a-fb57-4e1f-a41d-756de5f9255f"
+const server_ip = "172.20.10.2"
 
 const base_url = "https://localhost:10000";
 // const login_url = `https://localhost:7001/rest/v3/login/sessions`;
@@ -15,6 +16,6 @@ const events_viewer_url = 'https://localhost:10000/dashboard/eventsViewer'
 const mobile_connect_url = 'https://localhost:10000/dashboard/mobileConnect'
 const playback_view_url = 'https://localhost:10000/dashboard/playbackView'
 
-const sentry_url = 'https://172.20.10.2:10000/sentry'
+const sentry_url = `https://${server_ip}:10000/sentry`
 
 export { base_url, login_url , users_url , dashboard_url , events_viewer_url , mobile_connect_url , playback_view_url , sentry_url, devices_url , system_id, local_devices_url }
